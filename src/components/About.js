19ee64ced26ef85b979ccac254152b85aaafb7fd
“your_name” <zena.Wang@kcl.ac.uk>
2022-06-01 09:42:47 +0100

    finish digital-business-card

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function About() {
+    return (
+        <div className="about">
+            <h3 className="about-title">About</h3>
+            <p className="about-content">Zena is 21 years old, currently studying Computer Science in King's College London. Try to learn React and find a remote internship about front-end in China. Hope everything great!</p>
+        </div>
+    )
+}
---
+++ src/components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interests() {
+    return (
+        <div className="interests">
+        <h3 className="interests-title">Interests</h3>
+        <p className="interests-content">Listening to Music (RNB, Rock, Jazz and light music). Play Games (Overwatch). Watch anime</p>
+    </div>
+    )
+}
